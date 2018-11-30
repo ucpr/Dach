bd684424915723069a78d11b7b921f5cf5539cd7
nve3pd <uchihara1229@gmail.com>
2018-11-30 21:32:32 +0900

    feat: impl run proc

--- src/dach.nim
+++ src/dach.nim
@@ -1,1 +1,18 @@
-# dach.nim
+import asynchttpserver
+import asyncdispatch
+import tables
+
+import dach/[route]
+export route, Port
+
+proc run*(r: Router, p=Port(8080), address="") =
+  var server = newAsyncHttpServer()
+  proc cb(req: Request) {.async.} =
+    let
+      url = req.url.path
+      mh = $req.reqMethod
+      params = r.match(url, mh)
+    if url != "./favicon.ico":
+      await req.respond(Http200, params.callback())
+
+  waitFor server.serve(port=p, cb, address=address)
