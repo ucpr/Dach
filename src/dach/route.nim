9a798df1328021bf08e401f54f49ed48226dfccd
nve3pd <uchihara1229@gmail.com>
2018-11-30 20:35:08 +0900

    feat: impl match procedure

--- src/dach/route.nim
+++ src/dach/route.nim
@@ -4,9 +4,10 @@ import asyncdispatch
 import sequtils
 import strutils
 import tables
+import nre
 
-type cbProc = proc(): string
 type 
+  cbProc = proc(): string
   Router* = ref object
     endpoints*: Table[string,
                   Table[string, tuple[name: string, callback: cbProc]]]
@@ -33,10 +34,16 @@ proc addRule*(r: Router, url: string, httpMethod: string, name: string, callback
     r.endpoints[httpMethod] = initTable[string, tuple[name: string, callback: cbProc]]()
   r.endpoints[httpMethod][url] = (name: name, callback: callback)
 
-proc match*(r: Router, url: string, httpMethod: string): int =
-  # return callback & var
+proc match*(r: Router, url: string, httpMethod: string): tuple[callback: cbProc, params: Table[string, string]] =
+  #[ return callback & var ]#
   let
     url = url.strip().strip(chars={'/'}, leading=false)
-  if not r.endpoints.hasKey(httpMethod):
-    return 1
-  discard
+#  if not r.endpoints.hasKey(httpMethod):
+#    return nil
+  for rule in r.endpoints[httpMethod].keys():
+    var urlVars = matchUrlVars(rule, url)
+    if urlVars.matchf:
+      let
+        callback = r.endpoints[httpMethod][rule].callback
+      return (callback: callback, params: urlVars.params)
+  return (callback: nil, params: initTable[string, string]())
