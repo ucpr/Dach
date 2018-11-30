550a2b8cef97e7675a79ec929caeb7090a47d114
nve3pd <uchihara1229@gmail.com>
2018-12-01 00:05:16 +0900

    update: update test.

--- tests/test_dach.nim
+++ tests/test_dach.nim
@@ -1,23 +1,18 @@
 import dach
+import json
 import tables
 
 var r = newRouter()
 
-proc hoge(): string =
-  return "Hello World!"
+proc hoge(): respObj =
+  return Response("Hello World")
 
-proc param(): string =
-  return "YES"
+proc param(): respObj =
+  return JsonResponse(%*[{"hoge": 2, "kobashiri": "kawaii"}])
 
 r.addRule(url="/hello", httpMethod="GET", name="root", callback=hoge)
 r.addRule(url="/hoge/{poyo}/hello", httpMethod="GET", name="root", callback=param)
 r.addRule(url="/hoge/{poyo}", httpMethod="GET", name="root", callback=param)
 r.addRule(url="/hoge", httpMethod="GET", name="root", callback=param)
-
-#for i in r.endpoints["GET"].keys():
-#  echo i
-
-#let a = r.match("/hello", "GET")
-#echo a.callback()
 
 r.run()
