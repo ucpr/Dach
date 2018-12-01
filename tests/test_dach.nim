6bae4f03715d2c96e339dc9593360c7dab611747
nve3pd <uchihara1229@gmail.com>
2018-12-01 11:29:48 +0900

    fix: add proc args

--- tests/test_dach.nim
+++ tests/test_dach.nim
@@ -4,14 +4,14 @@ import tables
 
 var r = newRouter()
 
-proc hoge(): respObj =
+proc hoge(p: dachParams): respObj =
   return Response("Hello World")
 
-proc param(): respObj =
+proc param(p: dachParams): respObj =
   return JsonResponse(%*[{"hoge": 2, "kobashiri": "kawaii"}])
 
 r.addRule(url="/hello", httpMethod="GET", name="root", callback=hoge)
-r.addRule(url="/hoge/{poyo}/hello", httpMethod="GET", name="root", callback=param)
+r.addRule(url="/hoge/{poyo}/hello", httpMethod="GET", name="root", callback=hoge)
 r.addRule(url="/hoge/{poyo}", httpMethod="GET", name="root", callback=param)
 r.addRule(url="/hoge", httpMethod="GET", name="root", callback=param)
 
