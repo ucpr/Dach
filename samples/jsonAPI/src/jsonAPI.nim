d2138c8cf3e8dccdcc94b0b7dcb608bfa57bd696
nve3pd <uchihara1229@gmail.com>
2018-12-25 16:15:16 +0900

    chore: demo

--- samples/jsonAPI/src/jsonAPI.nim
+++ samples/jsonAPI/src/jsonAPI.nim
@@ -15,8 +15,13 @@ proc paramsResp(p: dachParams): respObj =
   let content = %*[{"params": [p["poyo"], p["hoge"]]}]
   return JsonResponse(content)
 
+proc postReq(p: dachParams): respObj =
+  let content = %*[{"languages": ["Nim", "Python3", "C++", "Go", "Rust"]}]
+  return JsonResponse(content)
+
 app.addRule("/hello", "GET", "hello", hello)
 app.addRule("/api/languages", "GET", "lang", jsonResp)
 app.addRule("/api/{poyo}/{hoge}", "GET", "params", paramsResp)
+app.addRule("/post", "POST", "po", postReq)
 
 app.run()
