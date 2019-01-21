edd6b5f0d6a1dc4b94bb450ffdc1964206611baf
nve3pd <uchihara1229@gmail.com>
2019-01-21 15:38:32 +0900

    change: tailored to PatriciaTrieTree

--- src/dach/route.nim
+++ src/dach/route.nim
@@ -1,79 +1,48 @@
-import json
-import asynchttpserver
-import asyncdispatch
-import sequtils
-import strutils
 import tables
-import nre
+import strutils
+
+import critbittree, response
 
 type 
-  respObj* = tuple[content: string, headers: HttpHeaders]
-  dachParams* = Table[string, string]
-  cbProc = proc(p: dachParams): respObj
-  Router* = ref object
-    endpoints*: Table[string,
-                  Table[string, tuple[name: string, callback: cbProc]]]
+  httpMethods* = enum
+    GET = "GET"
+    POST = "PUT"
+    PUT = "PUT"
+    HEAD = "HEAD"
+    DELEATE = "DELEATE"
+    OPTIONS = "OPTIONS"
 
-proc splitSlash(url: string): seq[string] =
-  #[ split by slash ]#
-  result = url.strip(chars={'/'}+Whitespace).split("/")
+#  CallBack = proc(): string
+#  Parameters = Table[string, string]
 
-proc matchUrlVars(rule, url: string): tuple[matchf: bool, params: dachParams] =
-  #[
-    ruleに基づいてparamaterの値をurlから取得する
-  ]#
-  let 
-    rule = splitSlash(rule)
-    url = splitSlash(url)
-  if rule.len != url.len:
-    return (matchf: false, params: initTable[string, string]())
+  Items = object
+    rule: string
+    name: string
+    httpMethod: httpMethods
+    callbacks: Table[httpMethods, CallBack]
+  #paramIndices: array 
 
-  var params = initTable[string, string]()
-  for i in countup(0, rule.len - 1):
-    if rule[i].startsWith("{") and rule[i].endsWith("}"):
-      let
-        key = rule[i].strip(chars={'{', '}'})
-        value = url[i]
-      echo key, " : ", value
-      params[key] = value
-      continue
-    if rule[i] != url[i]:
-      echo rule[i], " ", url[i]
-      # TODO: 今はいったん空のTableを返す
-      return (matchf: false, params: initTable[string, string]())
-  return (matchf: true, params: params)
+  Router* = CritBitTree[Items]
 
 proc newRouter*(): Router =
-  result = new(Router)
-  result.endpoints = initTable[string,
-                      Table[string, tuple[name: string, callback: cbProc]]]()
+  var router: CritBitTree[Items]
+  return router
 
-proc addRule*(r: Router, url: string, httpMethod: string, name: string, callback: cbProc) =
-  #[
-    add rule to Router.
-    TODO: "/"のみの場合
-          "すでにaddRuleされてたときのexception"
-  ]#
-  let
-    url = url.strip().strip(chars={'/'}, leading=false)
-    httpMethod = httpMethod.toUpperAscii()
-  if not r.endpoints.hasKey(httpMethod):
-    r.endpoints[httpMethod] = initTable[string, tuple[name: string, callback: cbProc]]()
-  r.endpoints[httpMethod][url] = (name: name, callback: callback)
+proc addRule*(r: var Router, rule, name: string, httpMethod: httpMethods, callback: CallBack) =
+  var item: Items
 
-proc notfound(p: dachParams): respObj =
-  return (content: "NOTFOUND", headers: newHttpHeaders())
+  if not r.hasKey(rule):
+    item.callbacks = initTable[httpMethods, CallBack]()
 
-proc match*(r: Router, url: string, httpMethod: string): tuple[matchf: bool, callback: cbProc, params: dachParams] =
-  #[ return callback & var ]#
-  let
-    url = url.strip().strip(chars={'/'}, leading=false)
-#  if not r.endpoints.hasKey(httpMethod):
-#    return nil
-  for rule in r.endpoints[httpMethod].keys():
-    var urlVars = matchUrlVars(rule, url)
-    if urlVars.matchf:
-      let
-        callback = r.endpoints[httpMethod][rule].callback
-      return (matchf: true, callback: callback, params: urlVars.params)
-  return (matchf: false, callback: nil, params: initTable[string, string]())
+  item.rule = rule
+  item.name = name
+  item.httpMethod = httpMethod
+  item.callbacks[httpMethod] = callback
+
+  r[rule] = item
+
+proc hasRule*(r: Router, rule: string, httpMethod: httpMethods): bool =
+  if (not r.hasKey(rule)) or (not r[rule].callbacks.hasKey(httpMethod)):
+    return false
+  else:
+    return true
