166dd8e2e8f719ae9f8874783a1b5efd34646470
nve3pd <ic151206@edu.okinawa-ct.ac.jp>
2019-01-24 01:45:17 +0900

    feat: add default value

--- src/dach/configrator.nim
+++ src/dach/configrator.nim
@@ -1,12 +1,21 @@
-import tables
+import nativesockets, tables
+
+const
+  DefaultDebug* = true
+  DefaultPort* = 8080
+  DefaultAddress* = "127.0.0.1"
+  DefaultLogFormat* = "[$levelname] $datetime : "
 
 type
-  ConfigValues = enum
-    Debug,
-    Port,
-    ServerName,
+  Configrator* = ref object
+    debug*: bool
+    port*: uint16
+    address*: string
+#    logFormat*: string
+#    logLevel*: Level
 
-  Config = ref object
+proc newConfigurator*(): Configrator =
+  result = new Configrator
 
-proc loadConfigFile() =
+proc loadConfigFile*(): Configrator =
   discard
