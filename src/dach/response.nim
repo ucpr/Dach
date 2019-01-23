22e83ddd5301382fb9e9030275f75291bc0a7f79
nve3pd <ic151206@edu.okinawa-ct.ac.jp>
2019-01-24 04:40:48 +0900

    feat: add JsonResponse proc
    contentのtypeがstringでも実行できるようにした

--- src/dach/response.nim
+++ src/dach/response.nim
@@ -10,7 +10,12 @@ type
 proc Response*(content: string, statuscode = Http200, headers = newHttpHeaders()): Resp =
   result = (statuscode: statuscode, content: content, headers: headers)
 
+proc JsonResponse*(content: string, statuscode = Http200): Resp =
+  let jsonNode = parseJson(content)
+  let headers = newhttpheaders([("content-type", "application/json")])
+  result = (statuscode: statuscode, content: $content, headers: headers)
+
 proc JsonResponse*(content: JsonNode, statuscode = Http200): Resp =
-  let headers = newHttpHeaders([("Content-Type", "application/json")])
+  let headers = newhttpheaders([("content-type", "application/json")])
   result = (statuscode: statuscode, content: $content, headers: headers)
 
