cd9bff2567163fe6ec2820bad5e1999f0f7bacab
nve3pd <ic151206@edu.okinawa-ct.ac.jp>
2019-01-24 17:14:42 +0900

    wip: use httpbeast

--- src/dach.nim
+++ src/dach.nim
@@ -1,23 +1,66 @@
-import asynchttpserver
 import asyncdispatch
 import tables
+import strformat
+import httpcore
 
-import dach/[route, response]
-export route, response, Port
+import httpbeast except run, Settings
+import options
 
-proc run*(r: Router, p=Port(8080), address="") =
-  var server = newAsyncHttpServer()
-  proc cb(req: Request) {.async.} =
+import dach/[route, response, configrator, logger]
+export route, response, httpcore
+
+type
+  Dach* = ref object
+    router: Router
+    config: Configrator
+    routeNames: Table[string, string]
+
+proc newDach*(): Dach =
+  result = new Dach
+
+  result.router = newRouter()
+  result.config = newConfigurator()
+  result.routeNames = initTable[string, string]()
+
+proc addRoute*(r: var Dach, rule, name: string) =
+  r.routeNames[name] = rule
+
+proc addView*(r: var Dach, name: string, hm: HttpMethod, cb: CallBack) =
+  if not r.routeNames.hasKey(name):
+    return
+    # raise exception
+  let rule = r.routeNames[name]
+  r.router.addRule(rule, hm, cb)
+
+proc run*(r: Dach) =
+  let
+#    server = newAsyncHttpServer()
+    port = r.config.port
+    address = r.config.address
+    settings = initSettings(Port(8080), address)
+
+  #if r.config.debug == true:
+  dachConsoleLogger()
+
+  proc handler(req: Request): Future[void] =
     let
-      url = req.url.path
-      mh = $req.reqMethod
-      params = r.match(url, mh)
-    if url != "./favicon.ico":
-      if not params.matchf:
-        await req.respond(Http404, "NotFound")
-      let ctx = params.callback(params.params)
-      if ctx.headers.len == 0:
-        await req.respond(Http200, ctx.content)
-      else:
-        await req.respond(Http200, ctx.content, ctx.headers)
-  waitFor server.serve(port=p, cb, address=address)
+      url = req.path.get()
+      hm = req.httpMethod.get()
+#      query = req.url.query
+#      protocol = req.protocol.orig
+
+    if r.router.hasRule(url, hm):
+      let 
+        resp = r.router.get(url, hm)()
+        statucode = resp.statuscode
+      info(fmt"{$hm} {url} {statucode}")
+      req.send(resp.statuscode, resp.content, $resp.headers)
+      #info(fmt"{}")
+    else:
+      info(fmt"{$hm} {url} {$Http404}")
+      req.send(Http404, "Not Found")
+
+  echo fmt"Running on {address}:{port}"
+
+  httpbeast.run(handler, settings)
+
