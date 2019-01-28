87b04b0446ae3ac19c00daff370b7dcdf1c92f64
nve3pd <ic151206@edu.okinawa-ct.ac.jp>
2019-01-29 04:36:13 +0900

    feat: impl Cookie type

--- src/dach/cookie.nim
+++ src/dach/cookie.nim
@@ -1,3 +1,19 @@
 import cookies
+import strutils
 
+type
+  Cookie* = seq[string]
 
+proc `[]=`*(ck: var Cookie, key, val: string) =
+  let s = setCookie(key, val, noName = true)
+  ck.add(s)
+
+if isMainModule:
+  block:
+    assert @["a=1", "b=2", "c=3"].joinCookies() == "a=1;b=2;c=3"
+
+  block:
+    let ck = new Cookie
+    ck["name"] = "taro"
+
+    assert ck[0] == "name=taro"
