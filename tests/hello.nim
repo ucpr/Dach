490e40cfd1bd3642a56ee11a4a0c067befcfc3ac
nve3pd <ic151206@edu.okinawa-ct.ac.jp>
2019-01-31 04:01:13 +0900

    chore: change using API

--- tests/hello.nim
+++ tests/hello.nim
@@ -2,16 +2,10 @@ import dach
 
 var app = newDach()
  
-proc index(): Resp =
-  return Response("Hello World!")
-
-proc user(): Resp =
-  return Response("Hello User!")
+proc index(ctx: DachCtx): Resp =
+  ctx.response("Hello World!")
 
 app.addRoute("/", "index")
-app.addRoute("/hoge", "hoge")
-
 app.addView("index", HttpGet, index)
-app.addView("hoge", HttpGet, user)
 
 app.run()
