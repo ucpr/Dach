c008727361df36a4a79a33829e9c4a9d800c692f
nve3pd <ic151206@edu.okinawa-ct.ac.jp>
2019-01-31 21:20:33 +0900

    feat: add types

--- src/dach/response.nim
+++ src/dach/response.nim
@@ -1,14 +1,24 @@
 import json
+import tables
 import httpcore
 
+import cookie
+
 type
-  Resp* = tuple[statuscode: HttpCode,
-              content: string,
-              headers: HttpHeaders]
-  CallBack* = proc (): Resp
+  Resp* = tuple
+    statuscode: HttpCode
+    content: string
+    headers: HttpHeaders
+
+  DachCtx* = ref object
+    statuscode*: HttpCode
+    headers*: HttpHeaders
+    cookie*: Cookie
+    query*: Table[string, string]
 
 proc Response*(content: string, statuscode = Http200, headers = newHttpHeaders()): Resp =
   result = (statuscode: statuscode, content: content, headers: headers)
+  CallBack* = proc (ctx: DachCtx): Resp
 
 proc JsonResponse*(content: string, statuscode = Http200): Resp =
   let jsonNode = parseJson(content)
@@ -18,4 +28,5 @@ proc JsonResponse*(content: string, statuscode = Http200): Resp =
 proc JsonResponse*(content: JsonNode, statuscode = Http200): Resp =
   let headers = newhttpheaders([("content-type", "application/json")])
   result = (statuscode: statuscode, content: $content, headers: headers)
+
 
