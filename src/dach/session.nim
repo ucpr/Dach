42680f260be94c12f8d65b53c3eb28653a6ab31d
nve3pd <ic151206@edu.okinawa-ct.ac.jp>
2019-02-01 09:31:25 +0900

    chore: change type

--- src/dach/session.nim
+++ src/dach/session.nim
@@ -1,11 +1,11 @@
 import db_mysql
 import strutils
 
-type
-  Session* = DbConn
+#type
+#  DbConn* = DbConn
 
-proc newSession*(connection, username, password, db: string, createTable: bool = true): Session =
-  ## Create Session instance
+proc newSession*(connection, username, password, db: string, createTable: bool = true): DbConn =
+  ## Create DbConn instance
   result = open(connection, username, password, db)
   discard setEncoding(result, "utf8mb4")
   
@@ -18,42 +18,43 @@ proc newSession*(connection, username, password, db: string, createTable: bool =
       val VARCHAR(32) NOT NULL
     )""")
 
-proc rawGet(db: Session, key: string): string =
+proc rawGet(db: DbConn, key: string): string =
   let key = dbQuote(key)
   result = db.getValue(sql"SELECT val FROM session WHERE k=?", key)
 
-proc `[]`*(db: Session, key: string): string =
+proc `[]`*(db: DbConn, key: string): string =
   ## Get value from session storage
-  db.rawGet(key)
+  ## "`hoge`" てきな状態で値が帰ってくるのでreplace
+  db.rawGet(key).replace("'", "")
 
-proc hasKey(db: Session, key: string): bool =
+proc hasKey*(db: DbConn, key: string): bool =
   # TODO: Fix
   if db.rawGet(key) == "":
     return false
   else:
     return true
 
-proc rawInsert(db: Session, key, value: string) =
+proc rawInsert(db: DbConn, key, value: string) =
   let
     key = dbQuote(key)
     value = dbQuote(value)
 #  db.tryInsertId(sql"INSERT INTO session (l, val) VALUES (?, ?)", key, value)
   db.exec(sql"INSERT INTO session (k, val) VALUES (?, ?)", key, value)
 
-proc update(db: Session, key, value: string) =
+proc update(db: DbConn, key, value: string) =
   let
     key = dbQuote(key)
     value = dbQuote(value)
   db.exec(sql"UPDATE session SET val=? WHERE k=?", value, key)
 
-proc `[]=`*(db: Session, key, value: string) =
+proc `[]=`*(db: DbConn, key, value: string) =
   ## Insert Key-Value in session. If key exists, update value.
   if db.hasKey(key):
     db.update(key, value)
   else:
     db.rawInsert(key, value)
 
-proc del*(db: Session, key: string) =
+proc del*(db: DbConn, key: string) =
   ## Delete Key-Value from session
   db.exec(sql"DELETE FROM session WHERE k=?", key)
 
