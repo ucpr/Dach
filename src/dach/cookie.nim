165b363dd71b11aed7b00b68c6cd8d04204dba3d
nve3pd <ic151206@edu.okinawa-ct.ac.jp>
2019-02-02 00:56:09 +0900

    feat: impl concat proc

--- src/dach/cookie.nim
+++ src/dach/cookie.nim
@@ -1,8 +1,10 @@
 import cookies
-import strutils
+import strutils, strtabs
+import tables
 
 type
   Cookie* = seq[string]
+  CookieTable* = Table[string, string]
 
 proc `[]=`*(ck: var Cookie, key, val: string) =
   let s = setCookie(key, val, noName = true)
@@ -10,6 +12,14 @@ proc `[]=`*(ck: var Cookie, key, val: string) =
 
 proc concat*(c: Cookie): string =
   result = join(c, ";")
+
+proc concat*(c: StringTableRef): string =
+  result = ""
+  for i in c.pairs:
+    result &= i.key & "=" & i.value & ";"
+
+proc pop*(key: string): string =
+  result = ""
 
 if isMainModule:
   block:
