288a39ee77cb8301e096aa3bd46e58c1e5073f70
nve3pd <ic151206@edu.okinawa-ct.ac.jp>
2019-02-12 04:53:48 +0900

    feat: use nest module instead of critbittree

--- src/dach/route.nim
+++ src/dach/route.nim
@@ -1,130 +1,107 @@
-import tables
-import strutils
 import httpcore
+import uri, strutils, macros
 
-import times
-import random
+import nest
 
-import critbittree, response
+#import response
+
+type CallBack = proc (): string 
 
 type 
-#  HttpMethod* = enum
-#    GET = "GET"
-#    POST = "PUT"
-#    PUT = "PUT"
-#    HEAD = "HEAD"
-#    DELEATE = "DELEATE"
-#    OPTIONS = "OPTIONS"
+  DachRouter* = Router[CallBack]
 
-#  CallBack = proc(): string
-#  Parameters = Table[string, string]
+proc newDachRouter*(): DachRouter =
+  ## create new DachRouter instance
+  result = newRouter[CallBack]()
 
-  Items = ref object
-    rule: string
-#    hm: HttpMethod
-    callbacks: Table[HttpMethod, CallBack]
-  #paramIndices: array 
+proc addRule*(r: var DachRouter, rule: string, hm: HttpMethod, cb: CallBack) =
+  ## mapping to router
+  r.map(cb, ($hm).toLower, rule)
 
-  Router* = CritBitTree[Items]
+macro get*(head, path, body: untyped): untyped =
+  ## Add rule to router
+  ##
+  ## .. code-block::nim
+  ##    import dach
+  ##    var app = newDach()
+  ##    
+  ##    app.get "/":
+  ##      ctx.response("Hello World")
+  ##    
+  ##    app.run()
+  var strBody: string
+  for i in body:
+    strBody.add(fmt"    {repr(i)}" & "\n")
 
-proc newRouter*(): Router =
-  var router: CritBitTree[Items]
-  return router
-
-proc addRule*(r: var Router, rule: string, hm: HttpMethod, callback: CallBack) =
-  if not r.hasKey(rule):
-    var item = new Items
-    item.callbacks = initTable[HttpMethod, CallBack]()
-    item.rule = rule
-    item.callbacks[hm] = callback
-    r[rule] = item
-  else:
-    r[rule].callbacks[hm] = callback
-
-proc hasRule*(r: Router, rule: string, hm: HttpMethod): bool =
-  if (not r.hasKey(rule)) or (not r[rule].callbacks.hasKey(hm)):
-    return false
-  else:
-    return true
-
-proc get*(r: Router, rule: string, hm: HttpMethod): CallBack =
-  result = r[rule].callbacks[hm]
-
-if isMainModule:
-  let l = @[
-    "/",
-    "/user",
-    "/user/profile",
-    "/user/ac",
-    "/user/account",
-    "/user/char",
-    "/user/hoge",
-    "/ascii/hoge",
-    "/ascii/name",
-    "/namespace",
-    "/lang",
-    "/lang/javascript",
-    "/lang/python",
-    "/lang/ruby",
-    "/lang/nim",
-    "/sechack365/user",
-    "/sechack365/user/name",
-    "/sechack365/user/age",
-    "/sechack365/user/profile",
-    "/sechack365/user/email",
-    "/sechack365/okinawa",
-    "/sechack365/ehime",
-    "/sechack365/kanagawa",
-    "/sechack365/hokkaido",
-    "/sechack365/hukuoka",
-    "/sechack365/user/hoge",
-    "/sechack365/user/name/hoge",
-    "/sechack365/user/age/hoge",
-    "/sechack365/user/profile/hoge",
-    "/sechack365/user/email/hoge",
-    "/sechack365/okinawa/hoge",
-    "/sechack365/ehime/hoge",
-    "/sechack365/kanagawa/nay",
-    "/sechack365/hokkaido/poen",
-    "/sechack365/hukuoka/fdsfs",
-
-    "/nums/one/nyan",
-    "/nums/tow",
-    "/nums/three/hoge",
-    "/nums/four",
-    "/nums/five",
-    "/nums/six/fdsf",
-    "/nums/seven/dfdsf",
-    "/nums/eight",
-    "/nums/nine",
-    "/nums/ten/fllll"
-    ]
-  var r = newRouter()
-
+  var mainNode: string = fmt"""
+block:
   proc cb(ctx: DachCtx): Resp =
-    ctx.response("Hello World")
+{strBody}
+  {repr(head)}.router.addRule(""{repr(path)}"", HttpGet, cb)"""
+  result = parseStmt(mainNode)
 
+macro post*(head, path, body: untyped): untyped =
+  ## Add rule to router
+  var strBody: string
+  for i in body:
+    strBody.add(fmt"    {repr(i)}" & "\n")
 
-  for p in l:
-    r.addRule(p, HttpGet, cb)
-#  echo "routing time: ", cpuTime() - dachold
+  var mainNode: string = fmt"""
+block:
+  proc cb(ctx: DachCtx): Resp =
+{strBody}
+  {repr(head)}.router.addRule(""{repr(path)}"", HttpPost, cb)"""
+  result = parseStmt(mainNode)
 
-  var list: seq[string] = l
-  shuffle(list)
- 
-  let dachold = cpuTime()
-  for p in list:
-    if not r.hasRule(p, HttpGet):
-      echo false
-  echo "hasRule time: ", cpuTime() - dachold
+macro put*(head, path, body: untyped): untyped =
+  ## Add rule to router
+  var strBody: string
+  for i in body:
+    strBody.add(fmt"    {repr(i)}" & "\n")
 
-  let old = cpuTime()
-#  for p in l:
-#    r.addRule(p, HttpGet, cb)
-#  echo "routing time: ", cpuTime() - old
-  for p in list:
-    for i in l:
-      if p == i:
-        break
-  echo "hasRule time: ", cpuTime() - old
+  var mainNode: string = fmt"""
+block:
+  proc cb(ctx: DachCtx): Resp =
+{strBody}
+  {repr(head)}.router.addRule(""{repr(path)}"", HttpPut, cb)"""
+  result = parseStmt(mainNode)
+
+macro head*(head, path, body: untyped): untyped =
+  ## Add rule to router
+  var strBody: string
+  for i in body:
+    strBody.add(fmt"    {repr(i)}" & "\n")
+
+  var mainNode: string = fmt"""
+block:
+  proc cb(ctx: DachCtx): Resp =
+{strBody}
+  {repr(head)}.router.addRule(""{repr(path)}"", HttpHead, cb)"""
+  result = parseStmt(mainNode)
+
+macro delete*(head, path, body: untyped): untyped =
+  ## Add rule to router
+  var strBody: string
+  for i in body:
+    strBody.add(fmt"    {repr(i)}" & "\n")
+
+  var mainNode: string = fmt"""
+block:
+  proc cb(ctx: DachCtx): Resp =
+{strBody}
+  {repr(head)}.router.addRule(""{repr(path)}"", HttpDelete, cb)"""
+  result = parseStmt(mainNode)
+
+macro options*(head, path, body: untyped): untyped =
+  ## Add rule to router
+  var strBody: string
+  for i in body:
+    strBody.add(fmt"    {repr(i)}" & "\n")
+
+  var mainNode: string = fmt"""
+block:
+  proc cb(ctx: DachCtx): Resp =
+{strBody}
+  {repr(head)}.router.addRule(""{repr(path)}"", HttpOptions, cb)"""
+  result = parseStmt(mainNode)
 
