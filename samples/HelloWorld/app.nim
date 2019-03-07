750a0c5bb220a59013ff797a04312758ec72d01b
nve3pd <ic151206@edu.okinawa-ct.ac.jp>
2019-03-08 01:37:59 +0900

    chore: update

--- samples/HelloWorld/app.nim
+++ samples/HelloWorld/app.nim
@@ -1,20 +1,8 @@
 import dach
-import json
 
 var app = newDach()
 
 app.get "/":
-  ctx.response("Hello World!")
-
-app.get "/json":
-  ctx.jsonResponse(%*{"name": "u_chi_ha_ra_"})
-
-## or
-
-proc hoge(ctx: DachCtx): Resp =
-  redirect("/")
-
-app.addRoute("/hoge", "hoge")
-app.addView("hoge", HttpGet, hoge)
+  result.content = response("Hello World!")
 
 app.run()
